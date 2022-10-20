53161dc170e3bba18ea4bedd2a4d4863de780e06
tarekmn <nytarek@gmail.com>
2022-10-20 09:21:31 -0500

    updated schema

--- db/query.sql
+++ db/query.sql
@@ -6,3 +6,14 @@
 SELECT * from department;
 SELECT * from role;
 SELECT * from employee;
+
+
+-- SELECT employee.id, employee.first_name, employee.last_name, role.title, role.department_id, role.salary, employee.manager_id
+-- FROM employee
+-- INNER JOIN department ON department.department_id = role.department_id 
+-- INNER JOIN role ON role.id = employee.role_id;
+
+SELECT employee.id AS id, employee.first_name AS first_name, employee.last_name AS last_name, role.title AS title, role.department_id AS department_id , role.salary AS salary, employee.manager_id AS manager_id
+FROM employee
+INNER JOIN department ON department.id = role.department_id 
+INNER JOIN role ON role.id = employee.role_id;
--- db/schema.sql
+++ db/schema.sql
@@ -8,18 +8,22 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT
-);
-
-
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
   manager_id INT
 );
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT ,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
