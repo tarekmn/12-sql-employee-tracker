1ab8d25bf71cd73597e8170c2c86af810c500bf5
tarekmn <nytarek@gmail.com>
2022-10-21 16:32:20 -0500

    altered seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,7 +15,7 @@ VALUES ("manager", 75000, 3),
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("kobe", "bryant", 4, 24),
-       ("lebron", "james", 3, 12),
-       ("trey", "young", 1, 8),
-       ("magic", "johnson", 2, 13);
+VALUES ("tom", "johnson", 4, 24),
+       ("sarah", "james", 3, 12),
+       ("eric", "young", 1, 8),
+       ("nick", "wilson", 2, 13);
